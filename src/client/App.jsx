254e893b4f86cc2e16da93f695c2cf6726d00a9e
Quentin McMillian <quentinmcmillian@gmail.com>
2021-04-06 00:37:02 -0400

    Setup Track and Index components

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
 import Navbar from './components/NavBar';
+import Index from './components/Index';
+
 // import "./styles/App.css";
 
 const App = () => {
   return (
-    <div>
-      <Navbar/>
-    </div>
+    <Router>
+      <React.Fragment>
+        <Navbar/>
+        <div className="container">
+          <Switch>
+            <Route
+              exact path="/"
+              component={Index}
+            />
+          </Switch>
+        </div>
+      </React.Fragment>
+    </Router>
   )
 };
 
