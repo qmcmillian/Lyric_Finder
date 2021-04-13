257fd77376b235c52dbd89d8865e2e64a8643814
Quentin McMillian <quentinmcmillian@gmail.com>
2021-04-12 20:40:31 -0400

    Refactored code

--- src/client/components/tracks/Tracks.js
+++ src/client/components/tracks/Tracks.js
@@ -7,34 +7,26 @@ const Tracks = () => {
   const [state] = useContext(Context);
   const { track_list, heading } = state;
 
-  return (
-    <Context.Consumer>
-      {value => {
-        const { track_list } = value[0];
-
-        if (track_list === undefined || track_list.length === 0) {
-          return <Spinner/>
-        } else {
-          return (
-            <React.Fragment>
-              <h1 className="text-center mb-4">{heading}</h1>
-                <div className="row">{
-                  track_list.map((item) =>{
-                    return (
-                      <Track
-                        key={item.track.track_id}
-                        track={item.track}
-                      />
-                    )
-                  })
-                }
-                </div>
-            </React.Fragment>
-          )
-        }
-      }}
-    </Context.Consumer>
-  )
+  if (track_list === undefined || track_list.length === 0) {
+    return <Spinner/>
+  } else {
+    return (
+      <React.Fragment>
+        <h1 className="text-center mb-4">{heading}</h1>
+          <div className="row">{
+            track_list.map((item) =>{
+              return (
+                <Track
+                  key={item.track.track_id}
+                  track={item.track}
+                />
+              )
+            })
+          }
+          </div>
+      </React.Fragment>
+    )
+  }
 }
 
 export default Tracks;
